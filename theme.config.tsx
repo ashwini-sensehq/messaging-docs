4038997141519555888d9989b7c4f18a81ab6b26
Ashwini Kemshetty <21ashwinirao@gmail.com>
2023-06-06 16:47:12 +0530

    fix: initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Messaging Documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Spaced-Out/sense/tree/stable/ui_server/src/components/messaging',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  docsRepositoryBase: 'https://github.com/Spaced-Out/sense/tree/stable/ui_server/src/components/messaging', // change once the documentation is done
 }
 
 export default config
