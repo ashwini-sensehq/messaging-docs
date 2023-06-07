ef4ed74beff262218b825ba9cadb00bd83cb28db
Ashwini Kemshetty <21ashwinirao@gmail.com>
2023-06-07 11:59:22 +0530

    adding config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const withNextra = require('nextra')({
-  output: 'export',
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  images:{
+    unoptimized: true
+  }
+})
